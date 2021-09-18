c23134bf2c0b6578f9afb93ca305968c45c142f7
YegorDovydenko <dovydenko.e93@mail.ru>
2021-09-18 18:06:59 +0300

    Add files via upload

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,24 +1,60 @@
-export const Pagination = ({usersPerPage, allElements, paginate}) => {
+// import { useState } from "react"
+
+export const Pagination = ({usersPerPage, allElements, paginate, pages, currentPage, setCurrentPage}) => {
     
-    const pageNumberds = []
+    const pageNumbers = []
+    // const [style, setStyle] = useState('')
 
     for(let i = 1; i <= Math.ceil(allElements/usersPerPage); i++){
-        pageNumberds.push(i)
+        pageNumbers.push(i)
+    }
+
+    const nextPage = () => {
+        if(currentPage === pages){
+            setCurrentPage(0)
+        }
+        setCurrentPage(pv => pv + 1)
+    }
+
+    const prevPage = () => {
+        if(currentPage === 1){
+            setCurrentPage(pages + 1)
+        }
+        setCurrentPage(pv => pv - 1)
     }
 
     return(
-        <div className="pagination">
-            <h3>Pages found:</h3>
-            <ul>
-                {
-                    pageNumberds.map(number => (
-                        <li key={number}>
-                            <a href="!#" onClick={() => paginate(number)}>{number}</a>
-                        </li>
-                    ))
-                }
-            </ul>
-        </div>
+        <>
+            <div className="pagination">
+                <h3>Pages found:</h3>
+                <ul>
+                    {
+                        pageNumbers.map(number => {
+                            let isActiveClass = false
+                            if(currentPage === number){
+                                isActiveClass = true
+                            }
+                            
+                            return(
+                                <li key={number}>
+                                    <a className={isActiveClass ? "active" : ""} href="!#" onClick={() => paginate(number)}>{number}</a>
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
+            </div>
+            {
+                allElements > usersPerPage
+                ?
+                <div>
+                    <button onClick={prevPage}>Prev</button>
+                    <button onClick={nextPage}>Next</button>
+                </div>
+                :
+                null
+            }
+        </>
     )
 
 }
--- src/components/TableHeader.js
+++ src/components/TableHeader.js
@@ -1,44 +1,44 @@
 import sortTr from '.././tr.png'
 
-export const TableHeader = ({sortById, sortByFirstName, sortByLastName, sortByEmail, sortByPhone, sortByState}) => {
+export const TableHeader = ({sortById, sortByFirstName, sortByLastName, sortByEmail, sortByPhone, sortByState, setSortElemtns}) => {
     return(
         <tr>
-                  <th className="firstRow" onClick={sortById} id="sortId">
-                    <div>
-                      <h3>Id</h3>
-                      <img src={sortTr} alt="sort"/>
-                    </div>
-                  </th>
-                  <th className="firstRow" onClick={sortByFirstName} id="sortFirstName">
-                    <div>
-                      <h3>First name</h3>
-                      <img src={sortTr} alt="sort"/>
-                    </div>
-                  </th>
-                  <th className="firstRow" onClick={sortByLastName} id="sortLastName">
-                    <div>
-                      <h3>Last name</h3>
-                      <img src={sortTr} alt="sort"/>
-                    </div>
-                  </th>
-                  <th className="firstRow" onClick={sortByEmail} id="sortEmail">
-                    <div>
-                      <h3>Email</h3>
-                      <img src={sortTr} alt="sort"/>
-                    </div>
-                  </th>
-                  <th className="firstRow" onClick={sortByPhone} id="sortPhone">
-                    <div>
-                      <h3>Phone</h3>
-                      <img src={sortTr} alt="sort"/>
-                    </div>
-                  </th>
-                  <th className="firstRow" onClick={sortByState} id="sortState">
-                    <div>
-                      <h3>State</h3>
-                      <img src={sortTr} alt="sort"/>
-                    </div>
-                  </th>
-                </tr>
+          <th className="firstRow" onClick={sortById} id="sortId" onMouseOver={() => setSortElemtns('down')}>
+            <div>
+              <h3>Id</h3>
+              <img src={sortTr} alt="sort"/>
+            </div>
+          </th>
+          <th className="firstRow" onClick={sortByFirstName} id="sortFirstName" onMouseOver={() => setSortElemtns('down')}>
+            <div>
+              <h3>First name</h3>
+              <img src={sortTr} alt="sort"/>
+            </div>
+          </th>
+          <th className="firstRow" onClick={sortByLastName} id="sortLastName" onMouseOver={() => setSortElemtns('down')}>
+            <div>
+              <h3>Last name</h3>
+              <img src={sortTr} alt="sort"/>
+            </div>
+          </th>
+          <th className="firstRow" onClick={sortByEmail} id="sortEmail" onMouseOver={() => setSortElemtns('down')}>
+            <div>
+              <h3>Email</h3>
+              <img src={sortTr} alt="sort"/>
+            </div>
+          </th>
+          <th className="firstRow" onClick={sortByPhone} id="sortPhone" onMouseOver={() => setSortElemtns('down')}>
+            <div>
+              <h3>Phone</h3>
+              <img src={sortTr} alt="sort"/>
+            </div>
+          </th>
+          <th className="firstRow" onClick={sortByState} id="sortState" onMouseOver={() => setSortElemtns('down')}>
+            <div>
+              <h3>State</h3>
+              <img src={sortTr} alt="sort"/>
+            </div>
+          </th>
+        </tr>
     )
 }
